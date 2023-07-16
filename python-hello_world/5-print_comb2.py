9a7239fc1a306ab0a590e624c94dc8c247a09a69
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-07-16 13:27:08 +0300

    dd

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for x in range(10):
-        if i != 9 and x != 9:
-            print("{:02d},".format(i), end=" ")
-        else:
-            print("{:02d}".format(i), end="\n")
+for i in range(100):
+    if i != 99:
+        print("{:02d},".format(i), end=" ")
+    else:
+        print("{:02d}".format(i), end="\n")

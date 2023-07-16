7cc2be267e512874a4296ac01a0247d50669cfca
mohmedEldeeb <eldeebmahammed@gmail.com>
2023-07-16 13:29:39 +0300

    ddd

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i != 99:
-        print("{:02d},".format(i), end=" ")
+for number in range(100):
+    if number < 99:
+        print("{:02d}, ".format(number), end="")
     else:
-        print("{:02d}".format(i), end="\n")
+        print("{:02d}".format(number))
